a91a1ad233d486b8b57ddb99daf31fd1ff9ec0f6
sudo213 <110729046+sudo213@users.noreply.github.com>
2023-04-02 17:14:36 +0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,7 @@
+export const config = {
+    // https://frigate-proxy.ru/?ref=8397880  - my ref :D
+    proxy: '5.188.141.174:10875:EksaAU:PUVessuvguF7', // ip:port:login:password
+    proxyLink: "https://mobileproxy.space/reload.html?proxy_key=e6a0709b4cc208700e79e0e93a2057ab",
+    proxyPerWallet: false, // true, to rotate IP for each wallet
+    oldWallets: false // true, to work with old wallets, paste mnemonics in file oldWallets.txt
+};
